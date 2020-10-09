73a9e2fe15a39f6fceffb32e26ef05c16ce3c53f
Naouali <elnaoualinebil@gmail.com>
2020-10-09 14:33:56 +0100

    retrive statrted

---
+++ retirive.c
@@ -0,0 +1,15 @@
+#include "glue.h"
+
+
+/**
+ * retrive - function to retrive a specific snap and replace the current
+ * version of the file
+ * @filename
+ * @version_number: no required
+ * Return: copy the specifid version an replace the main file
+ */
+
+void retrive(char *filename, int version)
+{
+
+}
