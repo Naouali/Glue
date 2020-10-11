673120584f8efa6421a1099f8470497f56d8bb20
Naouali <elnaoualinebil@gmail.com>
2020-10-11 18:58:02 +0100

    add

--- include/main.c
+++ include/main.c
@@ -6,10 +6,13 @@ int main(int argc, char **argv)
 {
     char *filename = argv[1];
     int check;
-    check = strcmp(argv[2], "ret");
-    if (check == 0)
+    if (argv[2] != NULL)
     {
-        ret(filename);
+        check = strcmp(argv[2], "ret");
+        if (check == 0)
+        {
+            ret(filename);
+        }
     }
     else {
         copy_file(filename);
